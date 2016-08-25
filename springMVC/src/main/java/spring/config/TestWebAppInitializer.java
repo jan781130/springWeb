ef55ef52e78d8319332f014e59396619de252f17
徐子然 <ggininder@gmail.com>
2016-08-25 20:16:38 +0800

    Create new Spring MVC project

---
+++ springMVC/src/main/java/spring/config/TestWebAppInitializer.java
@@ -0,0 +1,24 @@
+package spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class TestWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
